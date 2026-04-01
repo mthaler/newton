770295f0b1debcde890bae480931f16895e3358c
Michael Thaler <michael.thaler2@gmail.com>
2026-04-01 06:41:43 +0200

    Updated main.go.

--- main.go
+++ main.go
@@ -2,12 +2,32 @@ package main
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/mthaler/aparser"
 )
 
 func main() {
 	fmt.Println("Hello, World!")
+
+	var zeros []float64
+
+	for {
+		s := ""
+		fmt.Scanln(s)
+		if s == "" {
+			break
+		}
+
+		z, err := strconv.ParseFloat(s, 64)
+
+		zeros = append(zeros, z)
+		if err != nil {
+			panic(err)
+		}
+	}
+
+	fmt.Printf("%+v\n", zeros)
 
 	r, err := aparser.Eval("3 + 4")
 	if err != nil {
