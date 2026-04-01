b63690b1a3fdbc1ad701997c44f4067284a6c5d3
Michael Thaler <michael.thaler2@gmail.com>
2026-04-01 07:36:12 +0200

    Updated main.go.

--- main.go
+++ main.go
@@ -5,6 +5,8 @@ import (
 	"strconv"
 
 	"github.com/mthaler/aparser"
+	"github.com/mthaler/aparser/ast"
+	"github.com/mthaler/aparser/expr"
 )
 
 func main() {
@@ -12,6 +14,13 @@ func main() {
 
 	s := ""
 	fmt.Println("Please enter a function:")
+	fmt.Scanln(&s)
+	fmt.Println(s)
+	b := expr.CreateBuffer(s)
+	e := expr.ArithmeticExpression()
+	expr.Parse(e, b)
+	a := ast.CreateAST(b.Code.Code)
+	fmt.Printf("%+v\n", a)
 
 	for {
 		fmt.Println("Please enter a guess for the zero or c to continue:")
